a6e6660408a4fa07416465c5dbf1ec4c678dc5ee
Derek Ault <derekault@Dereks-MacBook-Pro.local>
2019-04-07 18:06:54 -0400

    Only allow user to vote once every 12 hours

--- IsItOpen/src/app/app.component.ts
+++ IsItOpen/src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   isOpen: boolean;
+  alreadyVoted: boolean;
+  openCount: number;
+  closedCount: number;
   title = 'Gay things';
 
   constructor(private elementRef: ElementRef) {
@@ -18,18 +21,34 @@ export class AppComponent implements OnInit {
   }
 
   getVotes() {
+    // Fake data until we get the backend set up
     this.isOpen = true;
+    this.openCount = 4;
+    this.closedCount = 2;
+    // Check to see if they have voted in that last 12 hours
+    const lastTimeVoted = JSON.parse(localStorage.getItem('lastTimeVoted'));
+    const diffInMs: number = Date.now() - lastTimeVoted;
+    const diffInHours: number = diffInMs / 1000 / 60 / 60;
+    if (diffInHours <= 12 && lastTimeVoted != null) {
+      this.alreadyVoted = true;
+    } else {
+      this.alreadyVoted = false;
+    }
     this.displayVotes();
   }
 
   displayVotes() {
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = this.isOpen ? '#7fc03f' : '#df5a54'; 
     this.title = this.isOpen ? 'Open' : 'Closed';
+    this.isOpen ? this.openCount++ : this.closedCount++;
   }
 
   vote(vote: boolean) {
     // Update count and change the color
     this.isOpen = vote;
+    this.alreadyVoted = true;
+    const date = Date.now();
+    localStorage.setItem('lastTimeVoted', JSON.stringify(date));
     this.displayVotes();
   }
 }
