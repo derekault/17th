cd88ef9789c96773a5a19c550ccedaa40d374460
Derek Ault <derekault@Dereks-MacBook-Pro.local>
2019-04-07 17:27:21 -0400

    bootstrap added, first iteration of the page done

--- IsItOpen/src/app/app.component.ts
+++ IsItOpen/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'IsItOpen';
+export class AppComponent implements OnInit {
+  isOpen: boolean;
+  title = 'Gay things';
+
+  constructor(private elementRef: ElementRef) {
+
+  }
+
+  ngOnInit() {
+    this.getVotes();
+  }
+
+  getVotes() {
+    this.isOpen = true;
+    this.displayVotes();
+  }
+
+  displayVotes() {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = this.isOpen ? '#7fc03f' : '#df5a54'; 
+    this.title = this.isOpen ? 'Open' : 'Closed';
+  }
+
+  vote(vote: boolean) {
+    // Update count and change the color
+    this.isOpen = vote;
+    this.displayVotes();
+  }
 }
